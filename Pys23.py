50e8e6de7503661ced3a2e645ef32d1d146bcd8a
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-16 20:01:29 +0530

    Update Pys23.py

--- Pys23.py
+++ Pys23.py
@@ -1,7 +1,9 @@
 n=int(input())
-if n==2 or n==3 or n==5 or n==7:
+s=0
+for i in range(2,n):
+    if n%i==0:
+        s=1 
+if s==0:
     print("yes")
-elif(n%2==0) or(n%3==0) or (n%5==0) or (n%7==0) or (n==1) or n<0:
-    print("no")
 else:
-    print("yes")
+    print("no")
