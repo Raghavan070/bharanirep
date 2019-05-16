9c9c9b9f2aeb335bb1c203cd0287a5ca07f8d596
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-16 21:07:15 +0530

    Rename Pys23.py to Pys26.py

---
+++ Pys26.py
@@ -0,0 +1,10 @@
+
+n,m=map(int,input().split())
+for i in range(n,m):
+    if i>1:
+        for j in range(2, i):
+            if (i % j) == 0:
+
+                break
+        else:
+                print(i,end=" ")
