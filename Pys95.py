48bc6f1242662680e0a7e622ff13338e7d7c9aac
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-19 12:57:11 +0530

    Update Pys95.py

--- Pys95.py
+++ Pys95.py
@@ -1,8 +1,8 @@
 s=str(input())
 for i in range(0,len(s)):
-    if i%2!=0:
+    if i%2==0:
         print(s[i],end="")
 print(end=" ")
 for i in range(0,len(s)):
-    if i%2==0:
+    if i%2!=0:
         print(s[i],end="")
