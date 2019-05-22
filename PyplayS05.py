c0fda9b832575796e394641eae5bf577c81463bc
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-22 18:24:26 +0530

    Update PyplayS05.py

--- PyplayS05.py
+++ PyplayS05.py
@@ -1,4 +1,4 @@
-r=['I','II','III','IV','V','VI','VII','VIII','XI','X','XI','XII','XII','XIII','XIV','XV','XVI','XVI','XVII','XVIII','XIX','XX']
+r=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
 i=input()
 if i in r:
     print(r.index(i)+1)
