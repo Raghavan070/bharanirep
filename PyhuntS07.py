b0a0281fa6304ab97c44732a8ad507cb96ebf5c1
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-23 15:05:59 +0530

    Update PyhuntS07.py

--- PyhuntS07.py
+++ PyhuntS07.py
@@ -1,7 +1,10 @@
 n=int(input())
 m=list(map(int,input().split()))
+s=[]
 for i in range(0,n):
     if i%2==0 and m[i]%2!=0:
-        print(m[i],end=" ")
+        s.append(m[i])
     elif i%2!=0 and m[i]%2==0:
-        print(m[i],end=" ")
+        s.append(m[i])
+for i in range (0, len(s)): 
+    print (s[i],end=" ")
