f29f4d58b54f747ccaeee61efe06c5ec41778ab9
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-23 13:31:49 +0530

    Create PyhuntS04.py

---
+++ PyhuntS04.py
@@ -0,0 +1,9 @@
+n=int(input())
+x=[]
+k=list(map(int,input().split()))
+for i in range(0,n-1):
+    for j in range(i+1,n):
+        if k[i]==k[j]:
+            x.append(k[j])
+z=list(set(k)^set(x))
+print(z[0])
