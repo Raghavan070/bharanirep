086366cbd111a3b87b4ba48bc62220e99baefd19
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-25 12:31:31 +0530

    Delete PyproS17.py

--- PyproS17.py
+++
@@ -1,9 +0,0 @@
-n,m=map(int,input().split())
-j=0
-x=list(map(int,input().split()))
-for i in range(0,n-1):
-    if x[i]==x[i+1]:
-        j=x[i]+x[i+1]
-        i=i+1
-if m==j: print("yes")
-else: print("no")
