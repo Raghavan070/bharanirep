6b3c6acdb0121f9ffc4c7dd6f52dbdbea7499e06
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-25 14:11:25 +0530

    Update PyproS23.py

--- PyproS23.py
+++ PyproS23.py
@@ -7,6 +7,6 @@ for i in range(0,len(n)):
         y+=1
     else:
         z+=1
-if x%2==0 and (y%2!=0 and (z==0 or z%2!=0)) and (z%2!=0 and (y==0 or y%2!=0)):
+if x%2==0 and ((y%2!=0 and (z==0 or z%2!=0)) or (z%2!=0 and (y==0 or y%2!=0))):
     print("yes")
 else: print("no")
