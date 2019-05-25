2ea66b5ad5b5b795b212edd9f45e13e3326fcb9e
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-25 12:05:14 +0530

    Delete Pypro19.py

--- Pypro19.py
+++
@@ -1,9 +0,0 @@
-k=int(input())
-x=[]
-for i in range(0,k):
-    s=list(map(int,input().split()))
-    for j in range(0,len(s)):
-        x.append(s[j])
-d=sorted(x)
-for i in range(0,len(d)):
-    print(d[i],end=" ")
