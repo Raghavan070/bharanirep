50e9db63c3d4b9011251e337ca19a0aba71d9c7b
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-26 13:10:04 +0530

    Create PyhuntS19.py

---
+++ PyhuntS19.py
@@ -0,0 +1,11 @@
+n,k=map(int,input().split())
+q=[]
+for i in range(0,n):
+    s=list(map(int,input().split()))
+    q.append(s)
+w=[]
+for i in range(0,len(q)-1):
+    w=list(set(q[i])&set(q[i+1]))
+    i=i+1
+for i in range(0,len(w)):
+    print(w[i],end=" ")
