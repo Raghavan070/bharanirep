09601a8096019b47090dad1f7d1dce9fdf7797c8
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-27 17:04:50 +0530

    Create PyhuntS37.py

---
+++ PyhuntS37.py
@@ -0,0 +1,20 @@
+n=list(map(str,input()))
+q=[]
+t=[]
+p=[]
+e=""
+s=w=0
+y=len(n)
+for i in range(0,len(n)):
+    e=n.pop(i)
+    q.extend(n)
+    n.insert(i,e)
+m=y-1
+for i in range(0,len(q),m):
+    t=q[i:m]
+    p=t[::-1]
+    if t==p:
+        s=s+1
+    m=m+(y-1)
+if s==1: print("YES")
+else: print("NO")
