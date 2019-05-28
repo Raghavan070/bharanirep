79c8f2890b4d93f17b2bbcb29405ad483902967b
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-05-28 15:36:01 +0530

    Update PyplayS12.py

--- PyplayS12.py
+++ PyplayS12.py
@@ -1,4 +1,11 @@
+# your code goes here 
 n,k=map(int,input().split())
 t=k+1
+g=[]
 l=list(map(int,input().split()))
-print(l[t:]+l[:t])
+g=(l[t:]+l[:t])
+for i in range(0,len(g)):
+	print(g[i],end=" ")
+
+
+
