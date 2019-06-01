52e4bd95902702aa7be4ebdcb38f81758b2fa997
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-01 21:31:28 +0530

    Create PyproS49.py

---
+++ PyproS49.py
@@ -0,0 +1,19 @@
+def div(x,b):
+    k=0
+    if x == b: k = k + 1
+    for i in range(0, len(x) // 2):
+        n = len(x) // 2
+        if x[:int(n)] * 2 == x:
+            k=k+1
+            x = x[:int(n)]
+    return k
+
+a=list(map(str,input()))
+b=list(map(str,input()))
+if len(a)>len(b):x=a
+else: x=b
+k=0
+n=1
+if x==a: k=div(x,b)
+else: k=div(x,a)
+print(k)
