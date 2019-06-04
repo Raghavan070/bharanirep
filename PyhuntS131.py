5b11698dc7bf9a712ac1c104df9208d2d21c1414
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-04 17:59:00 +0530

    Create PyhuntS131.py

---
+++ PyhuntS131.py
@@ -0,0 +1,17 @@
+# your code goes here
+n=int(input()) 
+l=list(map(int, input().split()))
+l=sorted(l)
+print (l)
+m=[]
+y=n
+i=0
+while len(m)<n:
+	if len(m)!=n:
+		m.append(l[y-1])
+		y=y-1
+	if len(m)!=n:
+		m.append(l[i])
+		i=i+1
+for i in range(0,len(m)):
+	print(m[i],end=" ")
