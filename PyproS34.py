c1eab315b9b9e2b250dfc58764309feebdcfce37
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-04 15:15:27 +0530

    Create PyproS34.py

---
+++ PyproS34.py
@@ -0,0 +1,13 @@
+n,k=map(int,input().split())
+l=[]
+c=0
+for i in range(0,min(n,k)):
+       m=list(map(str,input().split()))
+       l.append(m)
+for i in range(0,min(n,k)):
+    for j in range(0,max(n,k)-1):
+        if l[i][j]=='R' and l[i][j+1]=='R':
+            c=c+5
+        if l[i][j]=='G' and l[i][j+1]=='G':
+            c=c+3
+print(c)
