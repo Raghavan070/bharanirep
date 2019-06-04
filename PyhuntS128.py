546b418251db8095ae1b22d80d5cd7ce410a30fa
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-04 23:27:03 +0530

    Create PyhuntS128.py

---
+++ PyhuntS128.py
@@ -0,0 +1,11 @@
+n=input()
+a=[]
+for i in range(0,len(n)):
+    for j in range(len(n)-1,i,-1):
+        if n[i] == n[j]:
+            x = n[i:j + 1]
+            if x == x[::-1]:
+                a.append(x)
+a=a[::-1]
+for i in range(0,len(a)):
+    print(a[i])
