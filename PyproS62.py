e371c1314a40cccca7265484d2b3cabbf25f52ce
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-04 12:40:09 +0530

    Update PyproS62.py

--- PyproS62.py
+++ PyproS62.py
@@ -1,10 +1,13 @@
 n=list(input())
+l=[]
 for i in range(0,len(n)-1):
-    for j in range(i+1,len(n)):
+    for j in range(len(n)-1,i,-1):
         if n[i]==n[j]:
             x=n[i:j+1]
             if x==x[::-1]:
-                for k in range(i,j+1):
-                    n.pop(i)
+                if len(l)<len(x):
+                    l=[]
+                    for k in range(i,j+1):
+                        l.append(n[k])
             break
-print(len(n))
+print(len(n)-len(l))
