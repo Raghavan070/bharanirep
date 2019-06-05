44fe98fb373eee7e87d468929635b18d48c8050f
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-05 21:15:48 +0530

    Update PyproS03.py

--- PyproS03.py
+++ PyproS03.py
@@ -1,6 +1,16 @@
 p,m=list(map(str,input().split()))
 s=0
 if p==m: print(s)
+elif len(p)>len(m):
+    p=list(p)
+    m=list(m)
+    for i in range(0,len(m)):
+        for j in range(0,len(p)):
+            if p[j]==m[i]:
+                p.pop(j)
+                break
+    s=len(p)
+    print(s)
 else:
     for i in range(0, min(len(p), len(m))):
         if p[i] != m[i]:
