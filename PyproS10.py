7cae2728e23e10b34d52ca2c71a4b5ff1df2178b
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-05 20:54:13 +0530

    Update PyproS10.py

--- PyproS10.py
+++ PyproS10.py
@@ -2,7 +2,7 @@ n=int(input())
 s=0
 m=list(map(int,input().split()))
 for i in range(0,n-1):
-    for j in range(i+1,n):
+    for j in range(0,n):
         if m[i]<m[j]:
             s=s+(i+1)
 print(s)
