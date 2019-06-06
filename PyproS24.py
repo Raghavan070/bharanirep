a3417ca06b9d50e7bd7d5553deaa670d5c44d6a6
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-06 12:11:14 +0530

    Create PyproS24.py

---
+++ PyproS24.py
@@ -0,0 +1,10 @@
+from itertools import product
+k=int(input())
+q="01"
+d=[]
+d=list(product(str(q),repeat=k))
+for i in range(0,len(d)):
+    w=list(d[i])
+    for j in range(0,len(w)):
+        print(w[j],end="")
+    print("\r")
