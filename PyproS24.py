c86fa0a8199ef4025b1e026078c6a2664b555fe7
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-06 12:19:55 +0530

    Update PyproS24.py

--- PyproS24.py
+++ PyproS24.py
@@ -1,10 +1,14 @@
-from itertools import product
-k=int(input())
-q="01"
-d=[]
-d=list(product(str(q),repeat=k))
-for i in range(0,len(d)):
-    w=list(d[i])
-    for j in range(0,len(w)):
-        print(int(w[j]),end="")
-    print("\r")
+n=int(input())
+a=[]
+k=bin(2**n-1)[2::] 
+
+l=len(k)
+for i in range(0,2**n):
+	s=bin(i)[2::]
+	if len(s)<l:
+		a.append([s.count("1"),(l-len(s))*"0"+s])
+	else:
+		a.append([s.count("1"),s])	
+a.sort()
+for i in range(0,len(a)):
+	print(a[i][1])
