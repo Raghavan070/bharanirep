523921714050f5d9f6b81803e9d0bc7e2ddf3c0c
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-08 12:48:21 +0530

    Create PyhuntS55.py

---
+++ PyhuntS55.py
@@ -0,0 +1,8 @@
+def shift(m,n):
+    return m[n:]+m[:n]
+n,k=map(int,input().split())
+m=list(map(int,input().split()))
+for i in range(0,k):
+    m=shift(m,i+1)
+for i in range(0,n):
+    print(m[i],end=" ")
