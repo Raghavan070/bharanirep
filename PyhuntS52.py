d6ddd385f929e01634f66d8447d15f8cb9ecfd1c
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-08 12:42:44 +0530

    Update PyhuntS52.py

--- PyhuntS52.py
+++ PyhuntS52.py
@@ -1,8 +1,9 @@
 n = int(input())
 c = 0
 for i in range(n):
-    j = n-i
-    if((i%2 == 0 or j%2 == 0) and i+j == n):
+    j = n-i 
+    e=i+j
+    if((i%2 == 0 or j%2 == 0) and e == n):
         c+=1
 if(n==2):
     c =2
