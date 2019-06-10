71182051003c47ecdd5f85fa78437f8b5dcd94b9
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-10 12:43:37 +0530

    Create PyhuntS48.py

---
+++ PyhuntS48.py
@@ -0,0 +1,12 @@
+n=list(map(str,input()))
+k=list(map(str,input()))
+s=0
+q=0
+for i in range(0,len(n)-len(k)+1):
+    if n[i] == k[0] and n[i + 1] == k[1] and n[i + 2] == k[2]:
+        s = s + 1
+        q=n.index(n[i],i)
+if s==1: print(q)
+else:
+    s=0
+    print(s)
