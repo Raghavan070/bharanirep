8dd8e9359f04ce20b7394751405e13bfa4247ebe
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-10 13:19:30 +0530

    Create PyproS26.py

---
+++ PyproS26.py
@@ -0,0 +1,15 @@
+
+nn=int(input())
+l=list(map(int,input().split()))
+m=[]
+c=1
+for i in l:
+    if i not in m:
+        m.append(i)
+        
+
+for i in range(len(m)-1):
+	if m[i]<m[i+1]:
+		c=c+1
+print(c) 
+ 
