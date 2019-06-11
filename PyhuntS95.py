ad3ba5b776ce715f66b6db9885041b73d41fdaa9
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-11 22:37:15 +0530

    Create PyhuntS95.py

---
+++ PyhuntS95.py
@@ -0,0 +1,18 @@
+def prime(n):
+    s=0
+    if n==2: return 2
+    else:
+        for i in range(2,n):
+            if n%i==0: s=s+1
+    if s==0: return n
+n=int(input())
+p=[]
+x=0
+for i in range(2,n):
+    x=(prime(i))
+    if x!=None: p.append(x)
+if len(p)==0:
+    x=0
+    print(x)
+else:
+    for i in range(len(p)): print(p[i],end=" ")
