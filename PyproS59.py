13e9b8ecc940c0a2b93ab38674c62371600ae340
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-12 13:52:39 +0530

    Update PyproS59.py

--- PyproS59.py
+++ PyproS59.py
@@ -1,7 +1,12 @@
+# your code goes here 
 import sys
 n,l=list(map(str,input().split("|")))
 a=list(input())
 if len(n)!=len(l):
+	if len(n)<len(l):
+		z=l
+		l=n
+		n=z
     for i in range(0,len(a)):
         l=l+a[i]
         if len(n)==len(l):
