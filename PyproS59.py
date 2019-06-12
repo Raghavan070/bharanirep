9ffbe6099e0066153a6a62fd71b8aad134f591a0
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-12 14:31:29 +0530

    Update PyproS59.py

--- PyproS59.py
+++ PyproS59.py
@@ -1,17 +1,15 @@
 # your code goes here 
-import sys
+
 n,l=list(map(str,input().split("|")))
-a=list(input())
-if len(n)!=len(l):
-	if len(n)<len(l):
-		z=l
-		l=n
-		n=z
-    for i in range(0,len(a)):
-        l=l+a[i]
-        if len(n)==len(l):
-            print(n,end="")
-            print("|",end="")
-            print(l)
-            sys.exit()
-print("Impossible")
+a=(input())
+if len(n)<len(l) and len(n+a)==len(l):
+	n=n+a
+	print(n,end="")
+	print("|",end="")
+	print(l)
+elif len(n)>len(l) and len(n)==len(l+a):
+	l=l+a
+	print(n,end="")
+	print("|",end="")
+	print(l)
+else: print("Impossible")
