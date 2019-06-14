b700cef34dc316f558edd1c1f3b6dd73cb0c9140
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-14 13:45:43 +0530

    Update PyhuntS72.py

--- PyhuntS72.py
+++ PyhuntS72.py
@@ -1,7 +1,10 @@
+# your code goes here 
 m=list(map(str,input().split()))
 for i in range(0,len(m),2):
     y=list(m[i])
     x=''
-    for j in range(len(y)-1,-1,-1): x=x+y[j]
+    for j in range(len(y)-1,-1,-1):
+    	if y[j]!=".":
+    		x=x+y[j]
     m[i]=str(x)
 print(*m)
