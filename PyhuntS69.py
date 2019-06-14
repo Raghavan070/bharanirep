2abe5b638d9fa62939d9c2609cef016fbfd5b81f
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-14 11:08:57 +0530

    Update PyhuntS69.py

--- PyhuntS69.py
+++ PyhuntS69.py
@@ -1,5 +1,6 @@
 n=list(input())
-x=['.','c','o','m']
-if n.count('@')==1 and n.count('.')==1 and len(n[n.index('@'):n.index('.')])>=4 and len(n[0:n.index('@')])>=3 and (n[len(n)-4:len(n)])==x:
+x=['.','c','o','m'] 
+y=['g','m','a','i','l']
+if n.count('@')==1 and n.count('.')==1 and len(n[n.index('@'):n.index('.')])>=4 and len(n[0:n.index('@')])>=3 and (n[len(n)-4:len(n)])==x and (n[n.index('@')+1:n.index('.')])==y:
     print("YES")
 else: print("NO")
