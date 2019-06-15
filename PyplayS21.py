7861813f9d0506baefc02e68e69013805f1f80cb
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-15 20:37:14 +0530

    Update PyplayS21.py

--- PyplayS21.py
+++ PyplayS21.py
@@ -4,4 +4,5 @@ for i in range(3):
     d.append(p)
 if d[0][0]==d[1][0] and d[2][0]==d[1][0]: print("yes")
 elif d[0][1]==d[1][1] and d[2][1]==d[1][1]: print("yes")
+elif  d[0][0]==d[0][1] and d[1][0]==d[1][1] and d[2][0]==d[2][1]: print ("yes")
 else: print("no")
