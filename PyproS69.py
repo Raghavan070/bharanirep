bef8567aba2a71d6d91550050a6524593c3d6286
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-16 17:12:40 +0530

    Update PyproS69.py

--- PyproS69.py
+++ PyproS69.py
@@ -1,3 +1,4 @@
+#bharani
 n,k,l=map(int,input().split())
 q=list(map(int,input().split()))
 w=list(map(int,input().split()))
@@ -8,3 +9,5 @@ for i in range(n):
     e.append(l - q[i])
     e.append(abs(w[i] - q[i]) + (w[i] - l))
 print(max(e))
+
+
