7a8e0a198935b2030af3e631206adcbb12abe1ab
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-22 10:40:28 +0530

    Update PyplayS113.py

--- PyplayS113.py
+++ PyplayS113.py
@@ -1,3 +1,3 @@
 m=list(map(int,input().split("/")))
-if 0<m[0]<32 and 0<m[1]<13 and m[2]>1300 and len(m)==3: print("yes")
+if 0<m[0]<32 and 0<m[1]<13 and m[2]>1200 and len(m)==3: print("yes")
 else: print("no")
