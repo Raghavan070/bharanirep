85a41736424e741e88de15d8da2093d8e98710d8
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-23 20:16:21 +0530

    Update PyplayS130.py

--- PyplayS130.py
+++ PyplayS130.py
@@ -2,7 +2,7 @@ n=int(input())
 l=list(map(int,input().split()))
 q=[]
 for i in range(0,n):
-    if i%2==0:
+    if i%2==0 or l[i]%2==0:
         q.append(sum(l[0:i+1]))
     else: q.append(l[i])
 print(*q)
