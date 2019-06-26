04b6b22c2fd879eede0ed67fdf84d3e8f0da9f55
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-06-26 11:07:21 +0530

    Rename PyplayS138.py to PyplayS148.py

---
+++ PyplayS148.py
@@ -0,0 +1,15 @@
+import sys
+n,m=map(int,input().split())
+k=l=[]
+x=[]
+for i in range(0,m):
+    l=list(map(int,input().split()))
+    k.append(l)
+for i in range(0,m):
+    l=k[i]
+    l=l[::-1]
+    x=k[i+1:]
+    if l in x:
+        print("no")
+        sys.exit()
+print("yes")
