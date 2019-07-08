b1a91bdb6713c511ee07a36b67a123ba047f51b7
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-08 21:40:21 +0530

    Create PyplayS171.py

---
+++ PyplayS171.py
@@ -0,0 +1,9 @@
+a="and"
+t=""
+n=list(map(str,input().split()))
+for i in range(len(n)):
+    if n[i]==a and i-1>0 and i+1<len(n):
+        t=n[i-1]
+        n[i-1]=n[i+1]
+        n[i+1]=t
+print(*n)
