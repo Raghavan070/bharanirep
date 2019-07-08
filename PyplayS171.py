3d74e0d94ad812076a804cf2c67f52b73512946d
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-08 21:42:17 +0530

    Update PyplayS171.py

--- PyplayS171.py
+++ PyplayS171.py
@@ -2,7 +2,7 @@ a="and"
 t=""
 n=list(map(str,input().split()))
 for i in range(len(n)):
-    if n[i]==a and i-1>0 and i+1<len(n):
+    if n[i]==a and i-1>=0 and i+1<len(n):
         t=n[i-1]
         n[i-1]=n[i+1]
         n[i+1]=t
