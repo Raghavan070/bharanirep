0f1d5c65acfbe0cfc9a19fc2a33bd01930925efb
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-19 07:05:28 +0530

    Update PyplayS187.py

--- PyplayS187.py
+++ PyplayS187.py
@@ -1,6 +1,6 @@
 n,k=map(str,input().split())
 s=1
-for i in range(2,max(n,k)):
+for i in range(2,max(len(n),len(k))):
     if len(n)%i==0 and len(k)%i==0: s=s+1
 if s==1: print("yes")
 else: print("no")
