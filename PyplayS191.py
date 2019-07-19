dc54c45e6f917c2354b5880f61c2a1bba40e602b
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-19 07:29:46 +0530

    Update PyplayS191.py

--- PyplayS191.py
+++ PyplayS191.py
@@ -1,5 +1,10 @@
 # your code goes here
 a=list(map(int, input (). split ()))
 b=list(map(int, input (). split ()))
-if a==b[::-1]: print ("yes") 
+z=0
+b=b[::-1]
+for i in range(len(a)):
+	if a[i]==b[i]:
+		z=z+1
+if z==len(a): print("yes") 
 else: print("no")
