22d9a70c94092c02b8eb5f427b1fc1a65ce33ea5
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-21 16:45:38 +0530

    Create PyproS37.py

---
+++ PyproS37.py
@@ -0,0 +1,12 @@
+import sys
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+if n==1:
+    print(1)
+    sys.exit()
+for i in range(0,n):
+    if i==n-1: x=0
+    else: x=i+1
+    if (l[i]<l[i-1] and l[i]<l[x]) or (l[i]>l[i-1] and l[i]>l[x]): s=s+1
+print(s)
