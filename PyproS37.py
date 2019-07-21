51a41cd6b25d0caef82f65b0b51afc4c7186f51d
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-21 16:47:44 +0530

    Update PyproS37.py

--- PyproS37.py
+++ PyproS37.py
@@ -5,8 +5,6 @@ s=0
 if n==1:
     print(1)
     sys.exit()
-for i in range(0,n):
-    if i==n-1: x=0
-    else: x=i+1
+for i in range(1,n-1):
     if (l[i]<l[i-1] and l[i]<l[x]) or (l[i]>l[i-1] and l[i]>l[x]): s=s+1
 print(s)
