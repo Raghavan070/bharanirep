1b70216f547cc4ad4ece268a8f8735a565136c67
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-27 18:22:38 +0530

    Create PyhuntS74.py

---
+++ PyhuntS74.py
@@ -0,0 +1,13 @@
+n=list(input())
+k=list(input())
+x=""
+for i in range(0,min(len(n),len(k))):
+    s=""
+    for j in range(0, min(len(n), len(k))):
+        if n[i]==k[j]:
+            s=s+n[i]
+            i=i+1
+        elif len(s)==0: continue
+        else: break
+        if len(s)>=len(x): x=s
+print(x)
