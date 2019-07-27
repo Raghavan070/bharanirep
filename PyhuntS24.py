aee66a651b8c4ea11f2e329f8a33ae2552bfc1cb
Bharanij27 <50615705+Bharanij27@users.noreply.github.com>
2019-07-27 19:13:45 +0530

    Create PyhuntS24.py

---
+++ PyhuntS24.py
@@ -0,0 +1,19 @@
+# your code goes here
+import sys
+from itertools import combinations 
+n,k=map(int, input ().split()) 
+l=list(map(str, input ().split())) 
+l=sorted(l) 
+q=[]
+for i in range (0,len(l)):
+	if l[i]<k: q.append(str(l[i]))
+x=str("".join(q))
+z=list(combinations (x,2))
+for i in range(0,len(z)):
+	s=0
+	r=list(z[i])
+	for j in range(0,2): s=s+int(r[j])
+	if s==k:
+		print("YES")
+		sys.exit()
+print("NO")
